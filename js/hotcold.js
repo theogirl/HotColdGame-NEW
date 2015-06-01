11f6a29f5e2e37345d8408fdeb187187ed711acf
theogirl <theogirl@gmail.com>
2015-06-01 01:49:45 -0400

    new version-working but no feedback

---
+++ js/hotcold.js
@@ -0,0 +1,107 @@
+
+$(document).ready (function() {
+	$('.userinput').focus();
+
+
+//-----Global Variables------//
+	var userGuesses;
+	var userScore;
+	var userNumber;
+	var randomNumber;
+	var goodNumber;
+	var winner;
+
+//-----Reset values for each new game------//
+	newGame();
+
+//-----Loop thru this sequence until the user wins!---//
+
+	$('.userinput').keydown(function(event) { 
+		if (event.which == 13) { 
+			
+			if (!winner) { 
+				userNumber = $('.userinput').val();
+				goodNumber = validateNumber(userNumber); // validate the number - returns T or F
+				if (goodNumber) {
+					alert(userNumber);
+					winner = compareNumber(userNumber);
+					recordGuesses();
+					updateScore();
+					resetInput();
+
+				} //end goodNumber test
+			}//end winner test
+		}// end event
+	});//end keydown
+ 
+// -------FUNCTIONS---------//
+
+//-----Resets the default values for each new game and generates a new random number------//
+function newGame() {
+		userGuesses = [];
+		userScore = 0;
+		winner = false;
+		randomNumber = getRandomInt(1, 100); // generate a random integer btwn 1 and 100	
+} 
+
+
+//---Generates a new random number------//
+function getRandomInt(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
+
+
+function validateNumber(userInput) {
+
+				if (userNumber == '') { // make sure input is not empty
+						alert('Please enter something!');
+						return false;
+								}
+				else if (userNumber% 1 !== 0) { // make sure input is not a decimal value
+						alert('Please enter an integer!');
+						return false;
+								}
+				else if (isNaN(userNumber)) { // make sure input is not a string
+						alert('Please enter an actual number!');
+						return false;
+								}
+				else if ((userNumber < 1) || (userNumber > 100)) { // make sure input is not outside of specified range
+						alert('Please enter a number between 1 and 100 only!');
+						return false;
+								}
+				else {
+						return true;
+					} 
+} 
+
+function compareNumber(userInput) {
+
+			if (randomNumber === userNumber) {
+				return true;
+			}
+			else {
+				return false;
+			}
+} // end compareNumber function
+
+
+function updateScore() {
+	userScore++; // increase their attempts count by 1
+	$('.userScore').text(userScore);
+}
+
+function recordGuesses() {
+ // add user's guess to array
+	userGuesses.push(userNumber); // add user's guess to array
+	$('.userGuesses').text(userGuesses);
+}
+
+
+function resetInput() {
+	$('.userinput').val(''); // return input field to blank
+	$(this).focus();
+}
+
+
+});
